08876117911097a059b3f5bf8d761fff0191b18a
Derek Mendez <derekmenvale@gmail.com>
2026-04-02 16:09:18 -0400

    add total field and constructor to WantedResponse

--- src/main/java/org/example/fugitivefinder/model/WantedResponse.java
+++ src/main/java/org/example/fugitivefinder/model/WantedResponse.java
@@ -3,14 +3,14 @@ package org.example.fugitivefinder.model;
 import java.util.List;
 
 public class WantedResponse {
-
+    private int total;
     private List<WantedPerson> items;
 
-    public List<WantedPerson> getItems() {
-        return items;
-    }
-
-    public void setItems(List<WantedPerson> items) {
+    public WantedResponse(int total, List<WantedPerson> items) {
+        this.total = total;
         this.items = items;
     }
+
+    public int getTotal() { return total; }
+    public List<WantedPerson> getItems() { return items; }
 }
