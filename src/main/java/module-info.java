9bae76cb33c7b393b541f6fca5d73a410e5e2ce6
ahmaed <ahmaedthomas1920@gmail.com>
2026-04-03 17:35:28 -0400

    updated pom.xml, module-info.java and DashboardController to facilitate Map integration

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module org.example.fugitivefinder {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.gluonhq.maps;
 
-    opens org.example.fugitivefinder.view to javafx.fxml;
+    opens org.example.fugitivefinder.view to javafx.graphics, javafx.fxml;
     exports org.example.fugitivefinder.view;
 
     opens org.example.fugitivefinder.controller to javafx.fxml;
--- src/main/java/org/example/fugitivefinder/controller/DashboardController.java
+++ src/main/java/org/example/fugitivefinder/controller/DashboardController.java
@@ -1,4 +1,32 @@
 package org.example.fugitivefinder.controller;
 
+import com.gluonhq.maps.MapPoint;
+import com.gluonhq.maps.MapView;
+import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
+
+
 public class DashboardController {
+    @FXML
+    private StackPane mapContainer;
+
+    private MapView mapView;
+
+    public void initialize() {
+        mapView = new MapView();
+        MapPoint usaCenter = new MapPoint(39.8283, -98.5795);
+        mapView.setCenter(usaCenter);
+        mapView.setZoom(3);
+        mapView.addLayer(new MapController(new MapPoint(40.7506, -73.4290)));
+
+        // Marker in Huntington
+        mapView.addLayer(new MapController(new MapPoint(40.8682, -73.4257)));
+
+        // Marker in Babylon
+        mapView.addLayer(new MapController(new MapPoint(40.6959, -73.3257)));
+
+
+        mapContainer.getChildren().add(mapView);
+    }
 }
+
