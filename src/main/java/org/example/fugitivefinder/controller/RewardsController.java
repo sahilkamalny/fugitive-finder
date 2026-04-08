c09a24c103fafb1842389fd5f041bb18fb6290bc
darianne123 <darianneramos@icloud.com>
2026-04-08 14:01:20 -0400

    fixed files

--- src/main/java/org/example/fugitivefinder/controller/RewardsController.java
+++ src/main/java/org/example/fugitivefinder/controller/RewardsController.java
@@ -10,6 +10,8 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import org.example.fugitivefinder.model.WantedPerson;
 import org.example.fugitivefinder.viewModel.RewardsViewModel;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
 
 public class RewardsController {
 
@@ -34,6 +36,15 @@ public class RewardsController {
     private void handleSearch() {
         viewModel.filter();
         renderCards();
+    }
+    @FXML
+    private void goToDashboard(MouseEvent event) {
+        viewModel.goToDashboard((Node) event.getSource());
+    }
+
+    @FXML
+    private void goToUserProfile(MouseEvent event) {
+        viewModel.goToUserProfile((Node) event.getSource());
     }
 
     private void renderCards() {
