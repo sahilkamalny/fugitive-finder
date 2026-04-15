b27498fd98d63f286da38e73c674c60204501bf5
darianne123 <darianneramos@icloud.com>
2026-04-14 22:33:04 -0400

    updated module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,22 +1,21 @@
 module org.example.fugitivefinder {
+
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.web;
+
+    requires com.gluonhq.maps;
+    requires com.fasterxml.jackson.databind;
     requires org.json;
     requires java.net.http;
 
     exports org.example.fugitivefinder.view;
-    opens org.example.fugitivefinder.view to javafx.fxml;
-
     exports org.example.fugitivefinder.controller;
-    opens org.example.fugitivefinder.controller to javafx.fxml;
-
     exports org.example.fugitivefinder.service;
     exports org.example.fugitivefinder.viewModel;
+
+    opens org.example.fugitivefinder.view to javafx.fxml, javafx.graphics;
+    opens org.example.fugitivefinder.controller to javafx.fxml;
     opens org.example.fugitivefinder.viewModel to javafx.fxml;
-    requires com.gluonhq.maps;
-    requires com.fasterxml.jackson.databind;
-
-    opens org.example.fugitivefinder.view to javafx.graphics, javafx.fxml;
-    exports org.example.fugitivefinder.view;
-
 }
