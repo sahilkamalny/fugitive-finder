6bd8236e3f7dfb5adc48baa618e55bf1aec67b5e
Armaan Arora <107405392+NexaArmaan@users.noreply.github.com>
2026-04-16 23:28:44 -0400

    Delete django directory

--- django/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import get_wanted_persons, register, login, save_target
-
-urlpatterns = [
-    path("wanted/", get_wanted_persons),
-    path("register/", register),
-    path("login/", login),
-    path("save-target/", save_target),
-]
